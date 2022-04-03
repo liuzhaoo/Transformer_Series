22d79b628a33a67aa089add353cd955c13dbb71a
zhaoliu <415091lz@gmail.com>
2022-04-03 20:20:29 +0800

    net

---
+++ transformer/model/network.py
@@ -0,0 +1,29 @@
+import torch
+from torch import nn
+
+class PositionalEncoding(nn.Module):
+    """位置编码"""
+    def __init__(self,num_hiddens,dropout,max_len=1000):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(dropout)
+        self.p = torch.zeros((1,max_len,num_hiddens))
+        x = torch.arange(max_len,dtype=torch.float32).reshape(-1,1)
+        y = torch.pow(10000,torch.arange(0,num_hiddens,2,dtype=torch.float32)/num_hiddens)
+        c = x/y
+        self.p[:,:,0::2] = torch.sin(c)
+        self.p[:,:,1::2] = torch.cos(c)
+    def forward(self,X):
+        # 根据输入长度确定编码长度
+        X = X + self.p[:,:X.shape[1],:].to(X.device)
+        return self.dropout(X)
+
+
+class PositionWiseFFN(nn.Module):
+    """实际就是两个linear，进行了维度的转换"""
+    def __init__(self,ffn_num_input,ffn_num_hiddens,ffn_nun_outputs,**kwargs):
+        super(PositionWiseFFN, self).__init__()
+        self.dense1 = nn.Linear(ffn_num_input,ffn_num_hiddens)
+        self.dense2 = nn.Linear(ffn_num_hiddens,ffn_nun_outputs)
+        self.relu = nn.ReLU()
+    def forward(self,X):
+        return self.dense2(self.relu(self.dense1(X)))
